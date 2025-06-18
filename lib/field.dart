4670e0c09a2d6299b583196a7c625ebb36350e21
Quantompixel <schodllukas8@gmail.com>
2025-06-18 11:54:07 +0200

    changed design of tic tac toe field

--- lib/field.dart
+++ lib/field.dart
@@ -8,24 +8,46 @@ class Field extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
+    return GestureDetector(
+      onTap: () {
+        onPress();
+      },
       child: Container(
-        margin: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(10)),
+          boxShadow: [
+            BoxShadow(color: Color(0xFF965D3A)),
+            BoxShadow(
+              color: Color(0xFFCC8051),
+              spreadRadius: -1.0,
+              blurRadius: 3,
+              offset: Offset(0, 2),
+            ),
+          ],
+        ),
+        margin: EdgeInsets.all(5),
         child: Builder(
           builder: (context) {
             if (fieldType == -1) {
               return SizedBox.expand();
             } else if (fieldType == 0) {
-              return SvgPicture.asset('assets/icons/circle.svg');
+              return Padding(
+                padding: EdgeInsetsGeometry.all(15),
+                child: SizedBox.expand(
+                  child: SvgPicture.asset('assets/icons/circle.svg'),
+                ),
+              );
             } else {
-              return SvgPicture.asset('assets/icons/cross.svg');
+              return Padding(
+                padding: EdgeInsetsGeometry.all(15),
+                child: SizedBox.expand(
+                  child: SvgPicture.asset('assets/icons/cross.svg'),
+                ),
+              );
             }
           },
         ),
       ),
-      onPressed: () {
-        onPress();
-      },
     );
   }
 }
