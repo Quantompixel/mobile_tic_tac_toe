ac7188e9adf17e23bdf85f7b0d4a3fc32fdda233
Quantompixel <schodllukas8@gmail.com>
2025-06-18 15:51:40 +0200

    animated reset button

--- lib/reset_button.dart
+++ lib/reset_button.dart
@@ -1,16 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
-class ResetButton extends StatelessWidget {
+class ResetButton extends StatefulWidget {
   const ResetButton({super.key, required this.onPress});
   final Function onPress;
+
+  @override
+  State<ResetButton> createState() => _ResetButtonState();
+}
+
+class _ResetButtonState extends State<ResetButton> {
+  double _turns = 0.0;
+
+  void _animate() {
+    // rotate 0.1 turns (~36°) to the right
+    setState(() => _turns = 0.1);
+    // then rotate back after 150ms
+    Future.delayed(const Duration(milliseconds: 150), () {
+      if (mounted) setState(() => _turns = 0.0);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {
-        onPress();
+        widget.onPress();
         Feedback.forTap(context);
+        _animate();
       },
       child: Container(
         width: 50.0,
@@ -27,10 +44,15 @@ class ResetButton extends StatelessWidget {
           ],
         ),
         child: Center(
-          child: SvgPicture.asset(
-            'assets/icons/reset.svg',
-            width: 20,
-            height: 20,
+          child: AnimatedRotation(
+            turns: _turns,
+            duration: Duration(milliseconds: 150),
+            curve: Curves.easeOut,
+            child: SvgPicture.asset(
+              'assets/icons/reset.svg',
+              width: 20,
+              height: 20,
+            ),
           ),
         ),
       ),
